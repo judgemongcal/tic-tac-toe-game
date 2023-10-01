dc91ca7a13598c4c65a69ca8c7cd1e03482002b9
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-10-01 20:54:36 +0800

    Working on game init interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
 		fontFamily: {
 			outfit: ["Outfit", "sans-serif"],
 		},
+		colors: {
+			"dark-navy": "#1A2A33",
+			"semi-dark-navy": "#1F3641",
+			silver: "#A8BFC9",
+			"silver-hover": "#DBE8ED",
+			"light-blue": "#31C3BD",
+			"light-blue-hover": "#65E9E4",
+			"light-yellow": "#F2B137",
+			"light-yellow-hover": "#FFC860",
+		},
 	},
 	plugins: [],
 };
