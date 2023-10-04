86312aa925f74b5c826526bbe6d35b6a455a6a17
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-10-04 20:32:29 +0800

    Added reset button functionality

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,25 @@
 "use strict";
 const gameBoxArr = document.querySelectorAll(".game-box");
+const resetBtn = document.querySelector(".reset");
+resetBtn === null || resetBtn === void 0 ? void 0 : resetBtn.addEventListener("click", function () {
+    gameBoxArr.forEach((gameBox) => {
+        gameBox.innerHTML = "";
+    });
+});
 gameBoxArr.forEach((gameBox) => {
     gameBox === null || gameBox === void 0 ? void 0 : gameBox.addEventListener("click", function () {
+        gameBox.innerHTML = "";
         if (gameBox.id === "x-mark") {
             console.log("X");
+            gameBox.innerHTML = `
+            <img src="../assets/images/icon-x.svg" alt="" class="p-3" />
+            `;
         }
         else {
             console.log("O");
+            gameBox.innerHTML = `
+            <img src="../assets/images/icon-o.svg" alt="" class="p-3" />
+            `;
         }
     });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
 const gameBoxArr = document.querySelectorAll(".game-box");
+const resetBtn = document.querySelector(".reset");
+
+resetBtn?.addEventListener("click", function () {
+	gameBoxArr.forEach((gameBox) => {
+		gameBox.innerHTML = "";
+	});
+});
 
 gameBoxArr.forEach((gameBox) => {
 	gameBox?.addEventListener("click", function () {
+		gameBox.innerHTML = "";
 		if (gameBox.id === "x-mark") {
 			console.log("X");
+			gameBox.innerHTML = `
+            <img src="../assets/images/icon-x.svg" alt="" class="p-3" />
+            `;
 		} else {
 			console.log("O");
+			gameBox.innerHTML = `
+            <img src="../assets/images/icon-o.svg" alt="" class="p-3" />
+            `;
 		}
 	});
 });
