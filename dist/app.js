2929f01ec13b090692c71f9a423ff2a533079bb8
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-10-18 21:36:18 +0800

    Added function to toggle Modal and dynamically change inner text

--- dist/app.js
+++ dist/app.js
@@ -1,22 +1,22 @@
 "use strict";
-const cpuBtn = document.querySelector('.vs-cpu');
-const humanBtn = document.querySelector('.vs-player');
+const cpuBtn = document.querySelector(".vs-cpu");
+const humanBtn = document.querySelector(".vs-player");
 const gameBoxArr = document.querySelectorAll(".game-box");
 const resetBtn = document.querySelector(".reset");
 const optionX = document.querySelector(".x-option");
-const xPath = document.querySelector('.x-path');
-const oPath = document.querySelector('.o-path');
+const xPath = document.querySelector(".x-path");
+const oPath = document.querySelector(".o-path");
 const optionO = document.querySelector(".o-option");
 const turnMarker = document.querySelector(".turn-img");
 const userScoreEl = document.querySelector(".user-score");
 const drawScoreEl = document.querySelector(".draw-score");
 const oppScoreEl = document.querySelector(".opp-score");
-const resultModal = document.querySelector('.result-modal');
-const winnerP1 = document.querySelector('.winner');
-const markModal = document.querySelector('.modal-mark');
-const textModal = document.querySelector('.modal-text');
-const quitBtn = document.querySelector('.modal-quit');
-const nextRoundBtn = document.querySelector('.modal-next-round');
+const resultModal = document.querySelector(".result-modal");
+const winnerP1 = document.querySelector(".winner");
+const markModal = document.querySelector(".modal-mark");
+const textModal = document.querySelector(".modal-text");
+const quitBtn = document.querySelector(".modal-quit");
+const nextRoundBtn = document.querySelector(".modal-next-round");
 const global = {
     isOpponentHuman: false,
     isUserTurn: true,
@@ -34,42 +34,44 @@ const global = {
 const initMarks = () => {
     optionX === null || optionX === void 0 ? void 0 : optionX.addEventListener("click", function () {
         checkActive();
-        if (optionO === null || optionO === void 0 ? void 0 : optionO.classList.contains('bg-silver')) {
+        if (optionO === null || optionO === void 0 ? void 0 : optionO.classList.contains("bg-silver")) {
             optionO.classList.remove("bg-silver");
-            oPath.style.fill = '#A8BFC9';
+            oPath.style.fill = "#A8BFC9";
         }
         optionX.classList.add("bg-silver");
-        xPath.style.fill = '#1A2A33';
+        xPath.style.fill = "#1A2A33";
         localStorage.setItem("userMark", "x");
         localStorage.setItem("oppMark", "o");
     });
     optionO === null || optionO === void 0 ? void 0 : optionO.addEventListener("click", function () {
         checkActive();
-        if (optionX === null || optionX === void 0 ? void 0 : optionX.classList.contains('bg-silver')) {
+        if (optionX === null || optionX === void 0 ? void 0 : optionX.classList.contains("bg-silver")) {
             optionX.classList.remove("bg-silver");
-            xPath.style.fill = '#A8BFC9';
+            xPath.style.fill = "#A8BFC9";
         }
         optionO.classList.add("bg-silver");
-        oPath.style.fill = '#1A2A33';
+        oPath.style.fill = "#1A2A33";
         localStorage.setItem("userMark", "o");
         localStorage.setItem("oppMark", "x");
     });
-    cpuBtn === null || cpuBtn === void 0 ? void 0 : cpuBtn.addEventListener('click', function () {
-        localStorage.setItem('isOppHuman', 'false');
+    cpuBtn === null || cpuBtn === void 0 ? void 0 : cpuBtn.addEventListener("click", function () {
+        localStorage.setItem("isOppHuman", "false");
     });
-    humanBtn === null || humanBtn === void 0 ? void 0 : humanBtn.addEventListener('click', function () {
-        localStorage.setItem('isOppHuman', 'true');
+    humanBtn === null || humanBtn === void 0 ? void 0 : humanBtn.addEventListener("click", function () {
+        localStorage.setItem("isOppHuman", "true");
     });
 };
 const checkOpp = () => {
-    global.isOpponentHuman = JSON.parse((localStorage.getItem('isOppHuman') || ""));
+    global.isOpponentHuman = JSON.parse(localStorage.getItem("isOppHuman") || "");
     console.log(global.isOpponentHuman);
 };
 const assignMarks = () => {
     global.userMark = localStorage.getItem("userMark") || "";
     global.oppMark = localStorage.getItem("oppMark") || "";
-    if (global.oppMark === 'x') {
-        setTimeout(function () { cpuMove(); }, 800);
+    if (global.oppMark === "x") {
+        setTimeout(function () {
+            cpuMove();
+        }, 800);
     }
 };
 const fetchScores = () => {
@@ -110,12 +112,12 @@ const reset = () => {
             : (global.isUserTurn = false);
         checkTurn();
     });
-    console.log('Reset');
+    console.log("Reset");
 };
 gameBoxArr.forEach((gameBox) => {
     gameBox === null || gameBox === void 0 ? void 0 : gameBox.addEventListener("click", function () {
         if (gameBox.id) {
-            console.log('Invalid Move');
+            console.log("Invalid Move");
             return;
         }
         else {
@@ -138,9 +140,13 @@ gameBoxArr.forEach((gameBox) => {
         checkTurn();
         checkWinner();
         if (global.isOpponentHuman === false) {
-            if (isGameOver() === false && global.isUserWinner === false && global.isOppWinner === false) {
+            if (isGameOver() === false &&
+                global.isUserWinner === false &&
+                global.isOppWinner === false) {
                 console.log(isGameOver(), global.isUserWinner, global.isOppWinner);
-                setTimeout(function () { cpuMove(); }, 800);
+                setTimeout(function () {
+                    cpuMove();
+                }, 800);
             }
             else {
                 return;
@@ -154,19 +160,21 @@ const generateRandomNum = () => {
     return random;
 };
 const cpuMove = () => {
-    const randomNum = generateRandomNum();
-    console.log(randomNum);
-    if (gameBoxArr[randomNum].id && !isGameOver()) {
-        cpuMove();
-    }
-    else {
-        gameBoxArr[randomNum].innerHTML = `
+    if (!isGameOver()) {
+        const randomNum = generateRandomNum();
+        console.log(randomNum);
+        if (gameBoxArr[randomNum].id && !isGameOver()) {
+            cpuMove();
+        }
+        else {
+            gameBoxArr[randomNum].innerHTML = `
             <img src="../assets/images/icon-${global.oppMark}.svg" alt="" class="p-3" id=""/>
             `;
-        gameBoxArr[randomNum].id = `${global.oppMark}-mark`;
-        global.isUserTurn = true;
-        checkTurn();
-        checkWinner();
+            gameBoxArr[randomNum].id = `${global.oppMark}-mark`;
+            global.isUserTurn = true;
+            checkTurn();
+            checkWinner();
+        }
     }
 };
 // CHECK TURN
@@ -301,8 +309,10 @@ const checkWinner = () => {
         global.isUserWinner = false;
         global.isOppWinner = false;
     }
-    if (global.isUserWinner === true || global.isOppWinner === true || global.isDraw === true) {
-        console.log('Winner Found');
+    if (global.isUserWinner === true ||
+        global.isOppWinner === true ||
+        global.isDraw === true) {
+        console.log("Winner Found");
         GetWinner();
     }
     // }
@@ -347,6 +357,7 @@ const GetWinner = () => {
         }
         console.log(global);
         updateScore();
+        displayModal();
     }
     else {
         return;
@@ -354,19 +365,53 @@ const GetWinner = () => {
 };
 // DISPLAY MODAL
 const displayModal = () => {
-    resultModal.style.display = 'flex';
-    global.isUserWinner ? markModal.innerHTML = `<img src="/assets/images/icon-${global.userMark}.svg" alt="" class="w-[30px] lg:w-[64px]">` :
+    markModal.innerHTML = "";
+    textModal.innerHTML = "";
+    winnerP1.innerText = "";
+    if (global.isUserWinner) {
+        markModal.innerHTML = `<img src="/assets/images/icon-${global.userMark}.svg" alt="" class="w-[30px] lg:w-[64px]">`;
+        textModal.innerHTML = `<h1 class="text-[24px] lg:text-[40px] font-bold text-${global.userMark === "x" ? "light-blue" : "light-yellow"} ">TAKES THE ROUND</h1>`;
+    }
+    else if (global.isOppWinner) {
         markModal.innerHTML = `<img src="/assets/images/icon-${global.oppMark}.svg" alt="" class="w-[30px] lg:w-[64px]">`;
+        textModal.innerHTML = `<h1 class="text-[24px] lg:text-[40px] font-bold ${global.oppMark === "x" ? "text-light-blue" : "text-light-yellow"} ">TAKES THE ROUND</h1>`;
+        global.isOpponentHuman
+            ? (winnerP1.innerText = `PLAYER 2 WINS!`)
+            : (winnerP1.innerText = `OH NO, YOU LOST...`);
+    }
+    else if (!global.isOppWinner && global.isUserWinner) {
+        markModal.style.display = "hidden";
+        textModal.innerHTML = `<h1 class="text-[24px] lg:text-[40px] font-bold text-silver ">ROUND TIED/h1>`;
+    }
+    resultModal.style.display = "block";
+    quitBtn === null || quitBtn === void 0 ? void 0 : quitBtn.addEventListener("click", function () {
+        quitGame();
+    });
+    nextRoundBtn === null || nextRoundBtn === void 0 ? void 0 : nextRoundBtn.addEventListener("click", function () {
+        reset();
+        hideModal();
+        if (!global.isUserTurn && !global.isOpponentHuman) {
+            setTimeout(function () {
+                cpuMove();
+            }, 800);
+        }
+    });
+};
+const hideModal = () => {
+    resultModal.style.display = "none";
+};
+const quitGame = () => {
+    window.location.href = `http://127.0.0.1:5501/dist/index.html`;
 };
 const updateScore = () => {
     userScoreEl.innerText = `${global.userScore}`;
     oppScoreEl.innerText = `${global.oppScore}`;
     drawScoreEl.innerText = `${global.drawScore}`;
-    if (global.isUserWinner || global.isOppWinner || global.isDraw) {
-        setTimeout(function () {
-            reset();
-        }, 2000);
-    }
+    // if (global.isUserWinner || global.isOppWinner || global.isDraw) {
+    // 	setTimeout(function () {
+    // 		reset();
+    // 	}, 2000);
+    // }
 };
 const initGame = () => {
     gameBoxArr.forEach((gameBox) => {
@@ -375,7 +420,7 @@ const initGame = () => {
             image.style.visibility = "hidden";
         }
     });
-    resetBtn === null || resetBtn === void 0 ? void 0 : resetBtn.addEventListener('click', reset);
+    resetBtn === null || resetBtn === void 0 ? void 0 : resetBtn.addEventListener("click", reset);
     assignMarks();
     fetchScores();
 };
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-const cpuBtn = document.querySelector('.vs-cpu');
-const humanBtn = document.querySelector('.vs-player');
+const cpuBtn = document.querySelector(".vs-cpu");
+const humanBtn = document.querySelector(".vs-player");
 const gameBoxArr = document.querySelectorAll(".game-box");
-const resetBtn : HTMLButtonElement | null = document.querySelector(".reset");
+const resetBtn: HTMLButtonElement | null = document.querySelector(".reset");
 const optionX: HTMLDivElement | null = document.querySelector(".x-option");
-const xPath : HTMLOrSVGImageElement | null = document.querySelector('.x-path');
-const oPath : HTMLOrSVGImageElement | null = document.querySelector('.o-path');
+const xPath: HTMLOrSVGImageElement | null = document.querySelector(".x-path");
+const oPath: HTMLOrSVGImageElement | null = document.querySelector(".o-path");
 const optionO = document.querySelector(".o-option");
 const turnMarker = document.querySelector(".turn-img");
 const userScoreEl: HTMLParagraphElement | null =
@@ -13,15 +13,17 @@ const drawScoreEl: HTMLParagraphElement | null =
 	document.querySelector(".draw-score");
 const oppScoreEl: HTMLParagraphElement | null =
 	document.querySelector(".opp-score");
-const resultModal : HTMLDivElement | null = document.querySelector('.result-modal');
-const winnerP1 : HTMLParagraphElement | null = document.querySelector('.winner');
-const markModal : HTMLDivElement | null = document.querySelector('.modal-mark');
-const textModal : HTMLDivElement | null = document.querySelector('.modal-text');
-const quitBtn : HTMLButtonElement | null = document.querySelector('.modal-quit');
-const nextRoundBtn : HTMLButtonElement | null = document.querySelector('.modal-next-round');
+const resultModal: HTMLDivElement | null =
+	document.querySelector(".result-modal");
+const winnerP1: HTMLParagraphElement | null = document.querySelector(".winner");
+const markModal: HTMLDivElement | null = document.querySelector(".modal-mark");
+const textModal: HTMLDivElement | null = document.querySelector(".modal-text");
+const quitBtn: HTMLButtonElement | null = document.querySelector(".modal-quit");
+const nextRoundBtn: HTMLButtonElement | null =
+	document.querySelector(".modal-next-round");
 
 type Global = {
-	isOpponentHuman:boolean;
+	isOpponentHuman: boolean;
 	isUserTurn: boolean;
 	userMark: string;
 	oppMark: string;
@@ -50,44 +52,46 @@ const global: Global = {
 const initMarks = () => {
 	optionX?.addEventListener("click", function () {
 		checkActive();
-		if(optionO?.classList.contains('bg-silver')){
+		if (optionO?.classList.contains("bg-silver")) {
 			optionO.classList.remove("bg-silver");
-			oPath!.style.fill = '#A8BFC9';
+			oPath!.style.fill = "#A8BFC9";
 		}
 		optionX.classList.add("bg-silver");
-		xPath!.style.fill = '#1A2A33';
+		xPath!.style.fill = "#1A2A33";
 		localStorage.setItem("userMark", "x");
 		localStorage.setItem("oppMark", "o");
 	});
 	optionO?.addEventListener("click", function () {
 		checkActive();
-		if(optionX?.classList.contains('bg-silver')){
+		if (optionX?.classList.contains("bg-silver")) {
 			optionX.classList.remove("bg-silver");
-			xPath!.style.fill = '#A8BFC9';
+			xPath!.style.fill = "#A8BFC9";
 		}
 		optionO.classList.add("bg-silver");
-		oPath!.style.fill = '#1A2A33';
+		oPath!.style.fill = "#1A2A33";
 		localStorage.setItem("userMark", "o");
 		localStorage.setItem("oppMark", "x");
 	});
-	cpuBtn?.addEventListener('click', function (){
-		localStorage.setItem('isOppHuman', 'false');
-	})
-	humanBtn?.addEventListener('click', function(){
-		localStorage.setItem('isOppHuman', 'true');
-	})
+	cpuBtn?.addEventListener("click", function () {
+		localStorage.setItem("isOppHuman", "false");
+	});
+	humanBtn?.addEventListener("click", function () {
+		localStorage.setItem("isOppHuman", "true");
+	});
 };
 
 const checkOpp = () => {
-	global.isOpponentHuman = JSON.parse((localStorage.getItem('isOppHuman') || ""));
+	global.isOpponentHuman = JSON.parse(localStorage.getItem("isOppHuman") || "");
 	console.log(global.isOpponentHuman);
 };
 
 const assignMarks = (): void => {
 	global.userMark = localStorage.getItem("userMark") || "";
 	global.oppMark = localStorage.getItem("oppMark") || "";
-	if(global.oppMark === 'x'){
-		setTimeout(function(){cpuMove()}, 800);
+	if (global.oppMark === "x") {
+		setTimeout(function () {
+			cpuMove();
+		}, 800);
 	}
 };
 
@@ -117,8 +121,6 @@ const checkActive = () => {
 
 // RESET GAME
 
-
-
 const reset = () => {
 	gameBoxArr.forEach((gameBox) => {
 		const image = gameBox.querySelector("img");
@@ -135,79 +137,74 @@ const reset = () => {
 			: (global.isUserTurn = false);
 		checkTurn();
 	});
-	console.log('Reset');
-}
-
+	console.log("Reset");
+};
 
 gameBoxArr.forEach((gameBox) => {
 	gameBox?.addEventListener("click", function () {
-		if(gameBox.id){
-			console.log('Invalid Move');
+		if (gameBox.id) {
+			console.log("Invalid Move");
 			return;
-		} else{
-		gameBox.innerHTML = "";
-		if (global.isUserTurn) {
-			gameBox.innerHTML = `
+		} else {
+			gameBox.innerHTML = "";
+			if (global.isUserTurn) {
+				gameBox.innerHTML = `
             <img src="../assets/images/icon-${global.userMark}.svg" alt="" class="p-3" id=""/>
             `;
-			gameBox.id = `${global.userMark}-mark`;
-			global.isUserTurn = false;
-			
-			
-			
-		} else if (global.isOpponentHuman && !global.isUserTurn) {
-		
-			gameBox.innerHTML = `
+				gameBox.id = `${global.userMark}-mark`;
+				global.isUserTurn = false;
+			} else if (global.isOpponentHuman && !global.isUserTurn) {
+				gameBox.innerHTML = `
             <img src="../assets/images/icon-${global.oppMark}.svg" alt="" class="p-3"/>
             `;
-			gameBox.id = `${global.oppMark}-mark`;
-			global.isUserTurn = true;
-			
-		}
-	}
-
-	checkTurn();
-	checkWinner();
-		
-		if(global.isOpponentHuman === false){
-			if(isGameOver() === false && global.isUserWinner === false && global.isOppWinner === false ){
-				console.log(isGameOver(),global.isUserWinner, global.isOppWinner);
-				setTimeout(function(){cpuMove()}, 800);
+				gameBox.id = `${global.oppMark}-mark`;
+				global.isUserTurn = true;
 			}
-			else{
+		}
+
+		checkTurn();
+		checkWinner();
+
+		if (global.isOpponentHuman === false) {
+			if (
+				isGameOver() === false &&
+				global.isUserWinner === false &&
+				global.isOppWinner === false
+			) {
+				console.log(isGameOver(), global.isUserWinner, global.isOppWinner);
+				setTimeout(function () {
+					cpuMove();
+				}, 800);
+			} else {
 				return;
 			}
 		}
-		
-	
 	});
 });
 
-const generateRandomNum = () : number => {
+const generateRandomNum = (): number => {
 	const numOfBoxes = gameBoxArr.length;
-	const random = Math.abs(Math.floor(Math.random() * numOfBoxes-1));
+	const random = Math.abs(Math.floor(Math.random() * numOfBoxes - 1));
 	return random;
-}
+};
 
 const cpuMove = (): void => {
-
-	const randomNum = generateRandomNum();
-	console.log(randomNum);
-	if(gameBoxArr[randomNum].id && !isGameOver()){
-		cpuMove();
-	} else{
-		gameBoxArr[randomNum].innerHTML = `
+	if (!isGameOver()) {
+		const randomNum = generateRandomNum();
+		console.log(randomNum);
+		if (gameBoxArr[randomNum].id && !isGameOver()) {
+			cpuMove();
+		} else {
+			gameBoxArr[randomNum].innerHTML = `
             <img src="../assets/images/icon-${global.oppMark}.svg" alt="" class="p-3" id=""/>
             `;
-		gameBoxArr[randomNum].id = `${global.oppMark}-mark`;
-		global.isUserTurn = true;
-		checkTurn();
-		checkWinner();
+			gameBoxArr[randomNum].id = `${global.oppMark}-mark`;
+			global.isUserTurn = true;
+			checkTurn();
+			checkWinner();
+		}
 	}
-
-	
-	
-} 
+};
 
 // CHECK TURN
 const checkTurn = (): void => {
@@ -341,8 +338,12 @@ const checkWinner = (): void => {
 		global.isOppWinner = false;
 	}
 
-	if(global.isUserWinner === true || global.isOppWinner === true || global.isDraw === true){
-		console.log('Winner Found');
+	if (
+		global.isUserWinner === true ||
+		global.isOppWinner === true ||
+		global.isDraw === true
+	) {
+		console.log("Winner Found");
 		GetWinner();
 	}
 
@@ -362,17 +363,16 @@ const isGameOver = (): boolean => {
 		gameBoxArr[8].id != ""
 	) {
 		return true;
-	} else if (global.isUserWinner || global.isOppWinner || global.isDraw){
+	} else if (global.isUserWinner || global.isOppWinner || global.isDraw) {
 		return true;
-	}
-	 else {
+	} else {
 		return false;
 	}
 };
 
 // DISPLAY WINNER
 const GetWinner = (): void => {
-	if(isGameOver()){
+	if (isGameOver()) {
 		if (global.isUserWinner) {
 			console.log("Player 1 Wins!");
 			global.userScore++;
@@ -389,28 +389,69 @@ const GetWinner = (): void => {
 		}
 		console.log(global);
 		updateScore();
+		displayModal();
 	} else {
 		return;
 	}
 };
 
 // DISPLAY MODAL
-const displayModal = () : void => {
-	resultModal!.style.display = 'flex';
-	global.isUserWinner? markModal!.innerHTML = `<img src="/assets/images/icon-${global.userMark}.svg" alt="" class="w-[30px] lg:w-[64px]">` :
-	markModal!.innerHTML = `<img src="/assets/images/icon-${global.oppMark}.svg" alt="" class="w-[30px] lg:w-[64px]">`;
-}
+const displayModal = (): void => {
+	markModal!.innerHTML = "";
+	textModal!.innerHTML = "";
+	winnerP1!.innerText = "";
+
+	if (global.isUserWinner) {
+		markModal!.innerHTML = `<img src="/assets/images/icon-${global.userMark}.svg" alt="" class="w-[30px] lg:w-[64px]">`;
+		textModal!.innerHTML = `<h1 class="text-[24px] lg:text-[40px] font-bold text-${
+			global.userMark === "x" ? "light-blue" : "light-yellow"
+		} ">TAKES THE ROUND</h1>`;
+	} else if (global.isOppWinner) {
+		markModal!.innerHTML = `<img src="/assets/images/icon-${global.oppMark}.svg" alt="" class="w-[30px] lg:w-[64px]">`;
+		textModal!.innerHTML = `<h1 class="text-[24px] lg:text-[40px] font-bold ${
+			global.oppMark === "x" ? "text-light-blue" : "text-light-yellow"
+		} ">TAKES THE ROUND</h1>`;
+		global.isOpponentHuman
+			? (winnerP1!.innerText = `PLAYER 2 WINS!`)
+			: (winnerP1!.innerText = `OH NO, YOU LOST...`);
+	} else if (!global.isOppWinner && global.isUserWinner) {
+		markModal!.style.display = "hidden";
+		textModal!.innerHTML = `<h1 class="text-[24px] lg:text-[40px] font-bold text-silver ">ROUND TIED/h1>`;
+	}
+	resultModal!.style.display = "block";
+
+	quitBtn?.addEventListener("click", function () {
+		quitGame();
+	});
+	nextRoundBtn?.addEventListener("click", function () {
+		reset();
+		hideModal();
+		if (!global.isUserTurn && !global.isOpponentHuman) {
+			setTimeout(function () {
+				cpuMove();
+			}, 800);
+		}
+	});
+};
+
+const hideModal = (): void => {
+	resultModal!.style.display = "none";
+};
+
+const quitGame = (): void => {
+	window.location.href = `http://127.0.0.1:5501/dist/index.html`;
+};
 
 const updateScore = (): void => {
 	userScoreEl!.innerText = `${global.userScore}`;
 	oppScoreEl!.innerText = `${global.oppScore}`;
 	drawScoreEl!.innerText = `${global.drawScore}`;
 
-	if(global.isUserWinner || global.isOppWinner || global.isDraw){
-		setTimeout( function(){
-			reset();
-		}, 2000);
-	}
+	// if (global.isUserWinner || global.isOppWinner || global.isDraw) {
+	// 	setTimeout(function () {
+	// 		reset();
+	// 	}, 2000);
+	// }
 };
 
 const initGame = (): void => {
@@ -420,7 +461,7 @@ const initGame = (): void => {
 			image.style.visibility = "hidden";
 		}
 	});
-	resetBtn?.addEventListener('click', reset);
+	resetBtn?.addEventListener("click", reset);
 	assignMarks();
 	fetchScores();
 };
@@ -431,7 +472,7 @@ const InitApp = (): void => {
 		case "/dist/index.html":
 			console.log("Index");
 			initMarks();
-			
+
 			break;
 		case "/dist/game.html":
 			console.log("Game");
