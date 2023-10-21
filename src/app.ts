8106c5c5fbae0c12c842a7bca8384919642e12f6
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-10-21 21:24:32 +0800

    Added styling animation for winning combo

--- src/app.ts
+++ src/app.ts
@@ -136,6 +136,9 @@ const reset = () => {
 			image.style.visibility = "hidden";
 		}
 		gameBox.removeAttribute("id");
+		gameBox.classList.remove("bg-light-blue");
+		gameBox.classList.remove("bg-light-yellow");
+		gameBox.classList.add("bg-semi-dark-navy");
 	});
 
 	global.isUserWinner = false;
@@ -239,10 +242,26 @@ const animateCombo = (winningCombo: number[]) => {
 				gameBoxArr[number].classList.add(
 					global.userMark == "x" ? "bg-light-blue" : "bg-light-yellow",
 				);
+				gameBoxArr[number].innerHTML = "";
+				gameBoxArr[number].innerHTML =
+					global.userMark === "x"
+						? `<svg width="64" height="64" class='scale-[0.50]' xmlns="http://www.w3.org/2000/svg"><path d="M51.12 1.269c.511 0 1.023.195 1.414.586l9.611 9.611c.391.391.586.903.586 1.415s-.195 1.023-.586 1.414L44.441 32l17.704 17.705c.391.39.586.902.586 1.414 0 .512-.195 1.024-.586 1.415l-9.611 9.611c-.391.391-.903.586-1.415.586a1.994 1.994 0 0 1-1.414-.586L32 44.441 14.295 62.145c-.39.391-.902.586-1.414.586a1.994 1.994 0 0 1-1.415-.586l-9.611-9.611a1.994 1.994 0 0 1-.586-1.415c0-.512.195-1.023.586-1.414L19.559 32 1.855 14.295a1.994 1.994 0 0 1-.586-1.414c0-.512.195-1.024.586-1.415l9.611-9.611c.391-.391.903-.586 1.415-.586s1.023.195 1.414.586L32 19.559 49.705 1.855c.39-.391.902-.586 1.414-.586Z" stroke="#31C3BD" stroke-width="2" fill="#1A2A33"/></svg>`
+						: `<svg width="66" height="66" xmlns="http://www.w3.org/2000/svg"><path d="M33 1c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C15.327 65 1 50.673 1 33 1 15.327 15.327 1 33 1Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" stroke="#F2B137" stroke-width="2" fill="#1A2A33"/></svg>`;
+				// .querySelector("img")
+				// ?.setAttribute(
+				// 	"src",
+				// 	`../assets/images/icon-${global.userMark}-outline.svg`,
+				// );
 			} else {
 				gameBoxArr[number].classList.add(
 					global.oppMark == "x" ? "bg-light-blue" : "bg-light-yellow",
 				);
+				gameBoxArr[number]
+					.querySelector("img")
+					?.setAttribute(
+						"src",
+						`../assets/images/icon-${global.userMark}-outline.svg`,
+					);
 			}
 		});
 	}
